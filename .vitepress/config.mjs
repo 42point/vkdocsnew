7aaca3d8f770e141657526e8c65e43616a1cbdb1
Alexey S <lexx@42point.com>
2024-10-07 11:33:31 +0300

    fix metrika

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -14,6 +14,28 @@ export default defineConfig({
   //   },
   // },
   title: "База знаний",
+  head: [
+    [
+      "script",
+      {},
+      `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(97994354, "init", {
+           clickmap:true,
+           trackLinks:true,
+           accurateTrackBounce:true
+      });`,
+    ],
+    [
+      "noscript",
+      {},
+      `<div><img src="https://mc.yandex.ru/watch/97994354" style="position:absolute; left:-9999px;" alt="" /></div>`,
+    ],
+  ],
   description: "Помощь и поддежка vkurse.ru",
   // lastUpdated: true,
   themeConfig: {
